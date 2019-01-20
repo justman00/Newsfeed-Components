b867a8e914a254cee5056cb83232cba87fbb00aa
Vlad <vovaturcan4@gmail.com>
2019-01-21 00:18:15 +0200

    the menu slides from top and can disappear by clicking anywhere except the icon or the menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -8,4 +8,19 @@ const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", toggleMenu);
+menuButton.addEventListener("click", e => {
+  e.stopPropagation();
+  toggleMenu();
+});
+
+window.addEventListener("click", e => {
+  let res;
+  console.log(e.target);
+  if (e.target !== menuButton && e.target !== menu) {
+    if (
+      !Array.from(document.querySelectorAll(".menu ul li")).includes(e.target)
+    ) {
+      menu.classList.remove("menu--open");
+    }
+  }
+});
