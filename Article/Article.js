599e11535e439dd69432db682671af92c445467d
Vlad <vovaturcan4@gmail.com>
2019-01-21 19:15:57 +0200

    added a read button and the articles slide slowly in and out

--- Article/Article.js
+++ Article/Article.js
@@ -6,21 +6,55 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class.
     this.expandButton = domElement.querySelector(".expandButton");
+    // read button
+    this.readButton = domElement.querySelector(".readButton");
+    // console.log(this.readButton);
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.expandButton.textContent = "Click to Expand";
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle());
+    // event listener calling removeArticle
+    this.readButton.addEventListener("click", () => this.removeArticle());
+  }
+
+  removeArticle() {
+    this.domElement.classList.add("read");
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    articles.forEach(article => {
-      if (article.classList.contains("article-open")) {
-        article.classList.remove("article-open");
-      }
-    });
-    this.domElement.classList.toggle("article-open");
+    // articles.forEach(article => {
+    //   if (article.classList.contains("article-open")) {
+    //     article.classList.remove("article-open");
+    //   }
+    // });
+    // this.domElement.classList.toggle("article-open");
+    const arrArticles = Array.from(articles);
+    arrArticles.splice(arrArticles.indexOf(this.domElement), 1);
+    // console.log(arrArticles);
+    for (let i = 0; i < arrArticles.length; i++) {
+      arrArticles[i].classList.remove("article-open");
+      arrArticles[i].classList.add("article-close");
+    }
+    if (this.domElement.classList.contains("article-open")) {
+      this.domElement.classList.remove("article-open");
+      this.domElement.classList.add("article-close");
+      this.expandButton.textContent = "Click to Expand";
+    } else if (this.domElement.classList.contains("article-close")) {
+      this.domElement.classList.remove("article-close");
+      this.domElement.classList.add("article-open");
+      this.expandButton.textContent = "Click to Close";
+    } else {
+      // console.log("got here");
+      // console.log(this.domElement.classList);
+      this.domElement.classList.add("article-open");
+      this.expandButton.textContent = "Click to Close";
+    }
+    for (let i = 0; i < arrArticles.length; i++) {
+      arrArticles[i].querySelector(".expandButton").textContent =
+        "Click to Expand";
+    }
   }
 }
 
